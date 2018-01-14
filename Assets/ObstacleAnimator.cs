9cd2e9ec0ce50122a0ac9370d7e4cd57981493db
Bilal Itani <bilalitani1@gmail.com>
2018-01-14 15:24:20 -0500

    Set completed project files

---
+++ Assets/ObstacleAnimator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleAnimator : MonoBehaviour {
+
+    private Animator animatorComponent;
+    private Renderer rendererComponent;
+
+    private const string WARP_OUT_NAME = "WarpOut";
+    private const string WARP_IN_NAME = "WarpIn";
+
+    // Use this for initialization
+    void Start ()
+    {
+        animatorComponent = GetComponent<Animator>();
+        rendererComponent = GetComponent<Renderer>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            animatorComponent.Play(WARP_OUT_NAME);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            animatorComponent.Play(WARP_IN_NAME);
+        }
+    }
+
+    public void Disable()
+    {
+        rendererComponent.enabled = false;
+    }
+
+    public void Enable()
+    {
+        rendererComponent.enabled = true;
+    }
+}
